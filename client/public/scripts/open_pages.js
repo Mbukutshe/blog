543678fbcaef3ee266a5500f5f2d02e1644db1dc
Khanyisa Mbukutshe <khanyisam@bbd.co.za>
2020-03-04 15:11:25 +0200

    more

--- client/public/scripts/open_pages.js
+++ client/public/scripts/open_pages.js
@@ -1,6 +1,7 @@
 const posts = document.querySelector('#posts');
 const my_posts = document.querySelector('#mposts');
 const home = document.querySelector('#home');
+const more = document.querySelector('#more');
 
 posts.addEventListener('click', () => 
 {
@@ -10,6 +11,11 @@ posts.addEventListener('click', () =>
 my_posts.addEventListener('click', () => 
 {
     window.open('/user_posts', '_top');
+});
+
+more.addEventListener('click', () => 
+{
+    window.open('/single-posts', '_top');
 });
 
 home.addEventListener('click', () => 
--- client/public/scripts/pop.js
+++ client/public/scripts/pop.js
@@ -6,6 +6,40 @@ const forgot = document.querySelector('#forgot');
 const login_section = document.querySelector('#login-section');
 const register_section = document.querySelector('#register-section');
 const have_account = document.querySelector('#have-account');
+const login_btn = document.querySelector('#login-btn');
+const register_btn = document.querySelector('#register-btn');
+const message_label = document.querySelector('#message-label');
+
+
+login_btn.addEventListener('click', () => 
+{
+   // login_section.method = 'POST';
+  //login_section.action = '/user_posts';
+    
+ //   login_section.submit();
+});
+
+register_btn.addEventListener('click', () => 
+{
+    let username = document.getElementsByName('reg_user').value;
+    let email = document.getElementsByName('reg_email').value;
+    let password = document.getElementsByName('reg_pass').value;
+    let confpassowrd = document.getElementsByName('confpass').value;
+
+    if (password === confpassowrd)
+    {
+        //send the values to the server;
+    }
+    else
+    {
+        message_label.innerHTML = "Passwords do not match!";
+    }
+
+   // login_section.method = 'POST';
+  //login_section.action = '/user_posts';
+    
+ //   login_section.submit();
+});
 
 login.addEventListener('click', () => 
 {
@@ -20,11 +54,11 @@ close.addEventListener('click', () =>
 register.addEventListener('click', () =>
 {
     login_section.style.display = 'none';
-    register_section.style.display = 'block';
+    register_section.style.display = 'flex';
 });
 
 have_account.addEventListener('click', () =>
 {
-    login_section.style.display = 'block';
+    login_section.style.display = 'flex';
     register_section.style.display = 'none';
 });
